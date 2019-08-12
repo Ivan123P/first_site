d0956336c30bb976edeb313079884ef513801adb
Ivan Pavlenko <ip-xv_ix_xc@i.ua>
2019-08-12 22:35:08 +0300

    delete no need data

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,3 @@ let navItem = document.getElementsByClassName('js-nav-item');
     item.classList.add('active');
   }
 });
-
-const contactsBtn          = document.getElementById('contacts_btn');
-const contactsForm         = document.getElementById('contacts_form');
-const contactsFormName     = document.getElementById('contacts_name');
-const contactsFormNickName = document.getElementById('contacts_nickname');
-const contactsFormEmail    = document.getElementById('contacts_email');
-const contactsFormTextarea = document.getElementById('contacts_textarea1');
